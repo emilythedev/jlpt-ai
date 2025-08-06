f6b1f69c11f4dc3a86ef91e0d8f8333f00e58799
Emily L <emilythedev@gmail.com>
2025-08-06 18:57:44 +0900

    bookmark question

--- frontend/src/components/QuizFeedback.tsx
+++ frontend/src/components/QuizFeedback.tsx
@@ -1,8 +1,83 @@
+import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
-import type { QuestionFeedback } from '@/lib/types';
-import { useState } from 'react';
+import { db, type MultipleChoiceQuestionModel } from '@/lib/db';
+import type { Question, QuestionFeedback } from '@/lib/types';
+import { Bookmark, Check } from 'lucide-react';
+import { startTransition, useOptimistic, useState } from 'react';
 import QuestionCard from './QuestionCard';
+
+interface SaveToQuestionBankButtonProps {
+  question: Question;
+  selectedAnswer: string;
+}
+
+type QuestionSaveState = {
+  isSaved: boolean;
+  id?: number;
+};
+
+const SaveToQuestionBankButton = ({ question, selectedAnswer }: SaveToQuestionBankButtonProps) => {
+  const [saveState, setSaveState] = useState<QuestionSaveState>({ isSaved: false });
+  const [optimisticSaveState, setOptimisticSaveState] = useOptimistic(saveState,
+    (_, action: 'SAVE' | 'UNSAVE') => {
+      return  { isSaved: action === 'SAVE' };
+    }
+  );
+
+  const handleSave = async () => {
+    startTransition(() => setOptimisticSaveState('SAVE'));
+
+    try {
+      const questionId = await db.mc.add({
+        level: 'n3', // TODO: global setting
+        section: 'grammar',
+        question,
+        lastCorrectAt: selectedAnswer === question.correct_answer ? new Date() : undefined,
+      } as MultipleChoiceQuestionModel);
+
+      startTransition(() => setSaveState({ isSaved: true, id: questionId }));
+    } catch (err) {
+      console.log(err);
+      startTransition(() => setSaveState({ isSaved: false })); // rollback optimistic state
+    }
+  };
+
+  const handleUnsave = async () => {
+    startTransition(() => setOptimisticSaveState('UNSAVE'));
+
+    if (!saveState.id) return;
+
+    try {
+      await db.mc.delete(saveState.id);
+      startTransition(() => setSaveState({ isSaved: false }));
+    } catch (err) {
+      console.log(err);
+      startTransition(() => setSaveState(state => ({...state}))); // rollback optimistic state
+    }
+  };
+
+  if (optimisticSaveState.isSaved) {
+    return (
+      <Button
+        variant="ghost"
+        onClick={handleUnsave}
+      >
+        <Check />
+        保存した
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      onClick={handleSave}
+    >
+      <Bookmark />
+      後で復習
+    </Button>
+  );
+};
 
 interface QuizFeedbackProps {
   feedbacks: QuestionFeedback[];
@@ -13,6 +88,7 @@ const QuizFeedback = ({ feedbacks }: QuizFeedbackProps) => {
 
   const filteredFeedbacks = !showWrongAnswerOnly ? feedbacks :
     feedbacks.filter(q => q.correct_answer !== q.selectedAnswer);
+  // BUG: lost db state after save correct question and then toggle visibility
 
   return (
     <>
@@ -38,9 +114,14 @@ const QuizFeedback = ({ feedbacks }: QuizFeedbackProps) => {
             defaultValue={selectedAnswer}
             showResult
           >
-            <div className="mt-8 p-4 bg-green-100 text-green-800">
+            <div className="mt-8 mb-8 p-4 bg-green-100 text-green-800">
               {question.explanation}
             </div>
+
+            <SaveToQuestionBankButton
+              question={question}
+              selectedAnswer={selectedAnswer}
+            />
           </QuestionCard>
         ))}
       </div>
